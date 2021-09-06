7afccdde61a77fb34ac3aa9223a2c3eb1f412a68
Willian S <wsapcf@gmail.com>
2021-09-06 20:27:30 -0300

    end of drawer tile

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DrawerTile extends StatelessWidget {
-  const DrawerTile({Key key}) : super(key: key);
+  final IconData iconData;
+  final String title;
+
+  const DrawerTile({Key? key, required this.iconData, required this.title})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Icon(
+          iconData,
+          size: 32,
+          color: Colors.grey[700],
+        ),
+        Text(
+          title,
+          style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+        )
+      ],
+    );
   }
 }
