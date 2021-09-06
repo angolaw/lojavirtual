31d03033d6b02915732c3b34f2b671ac0638e475
Willian S <wsapcf@gmail.com>
2021-09-06 20:47:40 -0300

    add changeNotifierProvider on BaseScreen

---
+++ lib/models/page_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class PageManager extends ChangeNotifier {
+  final PageController pageController;
+
+  PageManager({required this.pageController});
+}
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
+import 'package:lojavirtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            title: Text("Home"),
+    return ChangeNotifierProvider(
+      create: (_) => PageManager(
+        pageController: pageController,
+      ),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: Text("Home"),
+            ),
+            drawer: CustomDrawer(),
           ),
-          drawer: CustomDrawer(),
-        ),
-        Container(
-          color: Colors.red,
-          child: ElevatedButton(
-              onPressed: () {
-                pageController.jumpToPage(1);
-              },
-              child: Text("Proximo")),
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],
+          Container(
+            color: Colors.red,
+            child: ElevatedButton(
+                onPressed: () {
+                  pageController.jumpToPage(1);
+                },
+                child: Text("Proximo")),
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
     );
   }
 }
