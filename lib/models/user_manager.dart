bdb69969f13bd860ff4ed4d15091ef484292d5e6
Willian S <wsapcf@gmail.com>
2021-09-09 19:51:37 -0300

    add callback function to signIn function:

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,17 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:lojavirtual/helpers/firebase_errors.dart';
 import 'package:lojavirtual/models/user.dart';
 
 class UserManager {
   final FirebaseAuth auth = FirebaseAuth.instance;
-  Future<void> signIn(User user) async {
+  Future<void> signIn(User user, Function onFail) async {
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
       print(result.user.uid);
     } on PlatformException catch (e) {
-      debugPrint(e.message);
+      onFail(getErrorString(e.code));
     }
   }
 }
