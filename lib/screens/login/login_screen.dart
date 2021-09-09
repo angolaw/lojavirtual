78936799a3ea2c319f6e2d645df06a9f8ca484c6
Willian S <wsapcf@gmail.com>
2021-09-09 18:51:22 -0300

    add appBar components to login screen

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Entrar"),
+        leading: const BackButton(),
+        actions: [const Center(child: Text("Criar conta"))],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Card(
+            child: ListView(
+          children: [TextFormField()],
+        )),
+      ),
+    );
+  }
+}
