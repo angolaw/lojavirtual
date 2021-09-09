18f310863595da2e02bdc875c889c8095203a599
Willian S <wsapcf@gmail.com>
2021-09-09 19:48:24 -0300

    add login call success

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,10 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:lojavirtual/models/user.dart';
 
 class UserManager {
   final FirebaseAuth auth = FirebaseAuth.instance;
   Future<void> signIn(User user) async {
-    final AuthResult result = await auth.signInWithEmailAndPassword(
-        email: user.email, password: user.password);
+    try {
+      final AuthResult result = await auth.signInWithEmailAndPassword(
+          email: user.email, password: user.password);
+      print(result.user.uid);
+    } on PlatformException catch (e) {
+      debugPrint(e.message);
+    }
   }
 }
