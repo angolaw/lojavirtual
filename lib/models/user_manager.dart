08e874184789e0cab42d95e73e251a687929006d
Willian S <wsapcf@gmail.com>
2021-09-09 19:36:05 -0300

    call signInWithEmailAndPassword from firebase auth'

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lojavirtual/models/user.dart';
 
 class UserManager {
-  void signIn(User user) {}
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  Future<void> signIn(User user) async {
+    final AuthResult result = await auth.signInWithEmailAndPassword(
+        email: user.email, password: user.password);
+  }
 }
