1ae75264cfc8e959f27afbcb7d12029b12bf7a47
Willian S <wsapcf@gmail.com>
2021-09-09 19:10:04 -0300

    add validators on form of login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/helpers/validators.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -13,55 +14,61 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
             margin: const EdgeInsets.symmetric(horizontal: 16),
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Email",
+            child: Form(
+              autovalidateMode: AutovalidateMode.always,
+              child: ListView(
+                padding: const EdgeInsets.all(16),
+                shrinkWrap: true,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    validator: (email) {
+                      if (!emailValid(email!)) {
+                        return "Email inválido";
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Senha",
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Senha",
+                    ),
+                    autocorrect: false,
+                    obscureText: true,
+                    validator: (password) {
+                      if (password!.isEmpty || password.length < 6) {
+                        return "Senha inválida!";
+                      }
+                      return null;
+                    },
                   ),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (password) {
-                    if (password!.isEmpty || password.length < 6) {
-                      return "Senha inválida!";
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text("Esqueci minha senha"),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  height: 44,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    child: const Text(
-                      "Entrar",
-                      style: TextStyle(fontSize: 18),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Esqueci minha senha"),
                     ),
                   ),
-                )
-              ],
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 44,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      child: const Text(
+                        "Entrar",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )),
       ),
     );
