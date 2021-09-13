c44604de469cd614be57221d32ca600a62307830
Willian S <wsapcf@gmail.com>
2021-09-13 11:21:39 -0300

    remove LoginScreen from drawer top

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
 import 'package:lojavirtual/models/page_manager.dart';
-import 'package:lojavirtual/screens/login/login_screen.dart';
 import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
@@ -16,7 +15,6 @@ class BaseScreen extends StatelessWidget {
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
         children: [
-          LoginScreen(),
           Scaffold(
             appBar: AppBar(
               title: Text("Home2"),
