84db7a2a9ccc2e02169380cb91811e7634896b5d
Willian S <wsapcf@gmail.com>
2021-09-13 11:47:34 -0300

    start of: add background gradient on drawer:

--- lib/common/custom_drawer/custom_drawer.dart
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -5,20 +5,25 @@ import 'package:lojavirtual/common/custom_drawer/drawer_tile.dart';
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          CustomDrawerHeader(),
-          const DrawerTile(iconData: Icons.home, title: "Inicio", page: 0),
-          const DrawerTile(iconData: Icons.list, title: "Produtos", page: 1),
-          const DrawerTile(
-              iconData: Icons.playlist_add_check,
-              title: "Meus Pedidos",
-              page: 2),
-          const DrawerTile(
-              iconData: Icons.location_on, title: "Lojas", page: 3),
-        ],
-      ),
+    return Stack(
+      children: [
+        Drawer(
+          child: ListView(
+            children: [
+              CustomDrawerHeader(),
+              const DrawerTile(iconData: Icons.home, title: "Inicio", page: 0),
+              const DrawerTile(
+                  iconData: Icons.list, title: "Produtos", page: 1),
+              const DrawerTile(
+                  iconData: Icons.playlist_add_check,
+                  title: "Meus Pedidos",
+                  page: 2),
+              const DrawerTile(
+                  iconData: Icons.location_on, title: "Lojas", page: 3),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
