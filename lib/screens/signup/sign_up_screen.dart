b147598334be84809276dc3cf98ccd5b74172978
Willian S <wsapcf@gmail.com>
2021-09-13 09:19:30 -0300

    create signup form

--- lib/screens/signup/sign_up_screen.dart
+++ lib/screens/signup/sign_up_screen.dart
@@ -8,6 +8,41 @@ class SignUpScreen extends StatelessWidget {
         title: const Text("Criar Conta"),
         centerTitle: true,
       ),
+      body: Center(
+        child: Card(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                    decoration:
+                        const InputDecoration(hintText: "Nome completo")),
+                const SizedBox(height: 16),
+                TextFormField(
+                    decoration: const InputDecoration(hintText: "Email")),
+                const SizedBox(height: 16),
+                TextFormField(
+                    decoration: const InputDecoration(hintText: "Senha")),
+                const SizedBox(height: 16),
+                TextFormField(
+                    decoration:
+                        const InputDecoration(hintText: "Repita a senha")),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {},
+                      child: const Text(
+                        "Criar conta",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
